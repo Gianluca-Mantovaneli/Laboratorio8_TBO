cc926b98643d406885e54849882988bbd7ba95c6
Gianluca-Mantovaneli <gianlucamantovaneli@gmail.com>
2024-08-13 15:53:45 -0300

    Refactor main.c to calculate and print the average height of the binary search tree

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main()
 {
     int N = 1000000; // Number of keys to generate
     int M = 10;      // Number of tests
-    double soma = 0;
+    double soma, soma2 = 0;
 
     // Create a new tree
     for (int i = 0; i < M; i++)
@@ -15,20 +15,25 @@ int main()
         // Generate the keys
         int *keys = generateKeys(N);
         Node *root = createNode(keys[0]);
+        Node *root2 = createNode(keys[0]);
 
         // Insert the keys into the tree
         for (int i = 0; i < N; i++)
         {
             root = insert(root, keys[i]);
+            root2 = insereWithRotation(root2, keys[i]);
         }
 
         soma += height(root);
+        soma2 += height(root2);
 
         // Delete the tree
         deleteTree(root);
+        deleteTree(root2);
         destroyKeys(keys);
     }
     printf("Average height: %f\n", soma / M);
+    printf("Average height with rotation: %f\n", soma2 / M);
 
     return 0;
 }
