e084e10e87e8286005a24032eb77d3e51ba409fe
Gianluca-Mantovaneli <gianlucamantovaneli@gmail.com>
2024-08-13 10:13:49 -0300

    Refactor main.c to fix insertion and deletion bugs in binary search tree

--- src/main.c
+++ src/main.c
@@ -5,15 +5,19 @@
 int main()
 {
     int N = 1000000; // Number of keys to generate
-    Node *root = createNode(rand() % 10000);
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < 10; i++)
     {
-        srand(time(0));
-        int key = rand() % 10000;
-        root = insert(root, key);
+        Node *root = createNode(rand() % 10000);
+        for (int j = 0; j < N; j++)
+        {
+            srand(time(0));
+            int key = rand() % 10000;
+            root = insert(root, key);
+        }
+        printf("Altura da árvore: %d\n", height(root));
+        deleteTree(root);
     }
-    printf("Altura da árvore: %d\n", height(root));
-    deleteTree(root);
+
     return 0;
 }
